6862496036aa64ca2cb2e09d77a18f861780bee8
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2022-09-07 18:44:19 -0300

    first commit

---
+++ src/TElementoAB.java
@@ -0,0 +1,80 @@
+public class TElementoAB<T> implements IElementoAB<T> {
+
+    private Comparable etiqueta;
+    private TElementoAB hijoIzq = null;
+    private TElementoAB hijoDer = null;
+    private T datos;
+
+    /**
+     * @param unaEtiqueta
+     * @param unosDatos
+     * @return 
+     */
+    @SuppressWarnings("unchecked")
+    public TElementoAB(Comparable unaEtiqueta, T unosDatos) {
+        etiqueta = unaEtiqueta;
+        datos = unosDatos;
+    }
+
+    @Override
+    public Comparable getEtiqueta() {
+        return etiqueta;
+    }
+
+    @Override
+    public TElementoAB<T> getHijoIzq() {
+        return hijoIzq;
+    }
+
+    @Override
+    public TElementoAB<T> getHijoDer() {
+        return hijoDer;
+    }
+
+    @Override
+    public void setHijoIzq(TElementoAB<T> elemento) {
+        hijoIzq = elemento;
+    }
+
+    @Override
+    public void setHijoDer(TElementoAB<T> elemento) {
+        hijoDer = elemento;
+    }
+
+    @Override
+    public TElementoAB<T> buscar(Comparable unaEtiqueta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean insertar(TElementoAB<T> elemento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String preOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String inOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String postOrden() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public T getDatos() {
+        return datos;
+    }
+
+    @Override
+    public TElementoAB eliminar(Comparable unaEtiqueta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   	
+}
